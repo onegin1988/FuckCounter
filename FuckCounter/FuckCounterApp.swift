f25cf20b847884240a0842ad394d7d4156b93bf8
alexandertaranenko <mjalexf1991@gmail.com>
2023-11-19 15:34:04 +0200

    Initial Commit

---
+++ FuckCounter/FuckCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuckCounterApp.swift
+//  FuckCounter
+//
+//  Created by Alex on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FuckCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
