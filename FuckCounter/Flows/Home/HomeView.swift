c6a9be675058ff5a9a587a6c0facddcf49d1bc85
alexandertaranenko <mjalexf1991@gmail.com>
2024-05-07 18:08:59 +0300

    removed vibrate

--- FuckCounter/Flows/Home/HomeView.swift
+++ FuckCounter/Flows/Home/HomeView.swift
@@ -98,7 +98,6 @@ struct HomeView: View {
             }
             .onReceive(dailyService.$timeSlice, perform: { _ in
                 homeViewModel.timeSlice = dailyService.timeSliceResult
-                AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
             })
             .onReceive(speechService.$speechRecognitionStatus, perform: { status in
                 switch status {
